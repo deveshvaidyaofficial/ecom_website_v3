a721f30f19346cd5c5de9c74a79d79e44d01dea4
deveshvaidyaofficial <deveshvaidya@outlook.com>
2026-07-08 17:19:27 +0530

    improved the file

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,12 @@
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./App.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById("root")).render(<App />);
+import App from "./App";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
