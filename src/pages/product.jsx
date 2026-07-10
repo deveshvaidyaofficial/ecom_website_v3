ec297ab08ddea4c2ab3c5e3271896fb7d9ed4136
deveshvaidyaofficial <deveshvaidya@outlook.com>
2026-07-10 16:57:27 +0530

    product page new fomrat updated

--- src/pages/product.jsx
+++ src/pages/product.jsx
@@ -1,19 +1,13 @@
 import { useParams } from "react-router-dom";
-
 import Navbar from "../components/Navbar/Navbar";
 import Footer from "../components/Footer/Footer";
-
 import products from "../data/products";
 
-function Product() {
 
+const Product = () => {
   const { product_id } = useParams();
-
-  const product = products.find(
-    p => p.id === Number(product_id)
-  );
-
-  if (!product) {
+  const product = products.find((p) => p.id === parseInt(product_id));
+    if (!product) {
     return <h2>Product Not Found</h2>;
   }
 
