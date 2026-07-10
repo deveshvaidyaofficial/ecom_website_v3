fa335c6ac4e9ba4600f8b8180dbe07468ed272ff
deveshvaidyaofficial <deveshvaidya@outlook.com>
2026-07-10 16:47:36 +0530

    minor fixes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route, Navigate } from "react-router-dom";
 
 import About from "./pages/About";
 import Product from "./pages/Product";
+import Home from "./pages/Home";
 
 function App() {
   return (
@@ -32,3 +33,5 @@ function App() {
 }
 
 export default App;
+
+//try to import everything eith control space formula
