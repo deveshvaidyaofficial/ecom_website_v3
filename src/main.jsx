8de9feacfb52679a084bdad55a4e2173cf007577
deveshvaidyaofficial <deveshvaidya@outlook.com>
2026-07-17 23:41:56 +0530

    v 8.8.2

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,6 @@ import { store } from "./redux/store";
 import App from "./App";
 import "./index.css";
 
-import { BrowserRouter } from "react-router-dom";
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Provider store={store}>
